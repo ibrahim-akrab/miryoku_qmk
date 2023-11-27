2ae821b69f9b67fc620e2e33380d51acc7d5f66d
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2023-11-27 12:03:39 +0200

    disable console debugging

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,6 +2,7 @@
 # https://github.com/manna-harbour/miryoku
 
 MIRYOKU_ALPHAS = QWERTY
-MIRYOKU_TAP=QWERTY
-MIRYOKU_NAV = VI
-SERIAL_DRIVER = bitbang
+MIRYOKU_TAP    = QWERTY
+MIRYOKU_NAV    = VI
+SERIAL_DRIVER  = bitbang
+CONSOLE_ENABLE = no
